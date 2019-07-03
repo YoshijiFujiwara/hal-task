ddf7db3061c2517cd8d86c21481270ff48220d7e
Yoshiji <yfbiology2@gmail.com>
2019-07-04 00:28:07 +0900

    設定内容をlocalStorageに保存して、永続化する

--- src/store/settings/actions.js
+++ src/store/settings/actions.js
@@ -1,3 +1,17 @@
-export const setShowPapersInOneList = ({ commit }, value) => {
+import { LocalStorage } from 'quasar'
+
+export const setShowPapersInOneList = ({ commit, dispatch }, value) => {
   commit('setShowPapersInOneList', value)
+  dispatch('saveSettings')
+}
+
+export const saveSettings = ({ state }) => {
+  LocalStorage.set('settings', state.settings)
+}
+
+export const getSettings = ({ commit }) => {
+  const settings = LocalStorage.getItem('settings')
+  if (settings) {
+    commit('setSettings', settings)
+  }
 }
--- src/store/settings/mutations.js
+++ src/store/settings/mutations.js
@@ -1,3 +1,7 @@
 export const setShowPapersInOneList = (state, value) => {
   state.settings.showPapersInOneList = value
 }
+
+export const setSettings = (state, settings) => {
+  Object.assign(state.settings, settings)
+}
